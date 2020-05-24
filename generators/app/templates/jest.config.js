3f7c63102580784fe43cbbf332a07e6e0352248a
TxHawks <tofu.hawks@gmail.com>
2020-05-24 12:07:07 +0300

    fix(app): jest config

--- generators/app/templates/jest.config.js
+++ generators/app/templates/jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
   ],
   testPathIgnorePatterns: [ '/node_modules/', '/.next/', ],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/jestTransformer.js',
   },
   transformIgnorePatterns: [
     '/node_modules/',
