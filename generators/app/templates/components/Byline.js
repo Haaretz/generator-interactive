c3e90d063adc6f5d1ccb009c0ae41d09f127464f
TxHawks <tofu.hawks@gmail.com>
2020-06-07 13:06:47 +0300

    feat(app): add byline component

---
+++ generators/app/templates/components/Byline.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { useFela, } from 'react-fela';
+
+import { useData, } from './DataContext';
+
+export default function Byline({
+  as = 'span',
+  wrapperStyles = null,
+  addressStyles = null,
+}) {
+  const writers = useData('writers');
+  const { css, } = useFela();
+  const Wrapper = as;
+
+  const wrapperClasses = wrapperStyles && css(wrapperStyles);
+  const addressClasses = addressStyles && css(addressStyles);
+
+  return (
+    <Wrapper className={wrapperClasses}>
+      {writers.map((writer, i) => {
+        const isLast = i === writers.length;
+        const hasMultipleWriters = writers.length > 1;
+
+        return (
+          <React.Fragment key={writer.contentName}>
+            <address className={addressClasses}>
+              {writer.url ? (
+                <a href={writer.url}>{writer.contentName}</a>
+              ) : (
+                writer.contentName
+              )}
+            </address>
+            {hasMultipleWriters ? (isLast ? ', ' : ' ו') : ''}
+          </React.Fragment>
+        );
+      })}
+    </Wrapper>
+  );
+}
