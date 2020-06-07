c8561b26a0fd01bc5f0bac6fa0c28cb01e4733b7
TxHawks <tofu.hawks@gmail.com>
2020-06-07 21:08:19 +0300

    fix(app): prevent polopoly log image from occcupying space

--- generators/app/templates/pages/[id]/[filename].js
+++ generators/app/templates/pages/[id]/[filename].js
@@ -76,7 +76,7 @@ export default function Page({ data, isClosed, site, } = {}) {
           type="text/javascript"
           // eslint-disable-next-line react/no-danger
           dangerouslySetInnerHTML={{
-            __html: `(function(){var logUrl='/logger/p.gif?a=${articleId}';var referrer=document.referrer;if(referrer&&referrer.length>0){logUrl+='&referrer='+referrer;}var img=document.createElement('img');img.src=logUrl;document.body.appendChild(img);})()`,
+            __html: `(function(){var logUrl='/logger/p.gif?a=${articleId}';var referrer=document.referrer;if(referrer&&referrer.length>0){logUrl+='&referrer='+referrer;}var img=document.createElement('img');img.src=logUrl;img.style.position='absolute';document.body.appendChild(img);})()`,
           }}
         />
       </DataProvider>
