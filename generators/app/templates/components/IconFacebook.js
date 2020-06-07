035d1ef206297d06e6cf06418470cfc7ad486225
TxHawks <tofu.hawks@gmail.com>
2020-06-07 12:42:49 +0300

    update icons

--- generators/app/templates/components/IconFacebook.js
+++ generators/app/templates/components/IconFacebook.js
@@ -14,16 +14,17 @@ export default function IconFacebookLogo({
 
   return (
     <svg
-      width="1em"
+      xmlns="http://www.w3.org/2000/svg"
       height="1em"
-      viewBox="0 0 256 256"
+      width="1em"
+      viewBox="0 0 310 310"
       className={className}
       {...props}
       {...attrs}
     >
       <path
         fill="currentColor"
-        d="M211 59v143.1c0 4.9-4 8.9-8.9 8.9H165v-63h18.7l2.7-24H165v-16.3c0-5.7 2.9-7.7 5.5-7.7H187V76h-21.6c-22.9 0-27.4 18.2-27.4 30.1V124h-15v24h15v63H59.8c-6 0-10.8-4.9-10.8-11V59.7C49 53.8 53.8 49 59.6 49H201c5.5 0 10 4.5 10 10z"
+        d="M81.7 165.1h34V305a5 5 0 005 5h57.6a5 5 0 005-5V165.8h39a5 5 0 005-4.5l6-51.5a5 5 0 00-5-5.5h-45V72c0-9.8 5.2-14.7 15.6-14.7h29.4a5 5 0 005-5V5a5 5 0 00-5-5h-42.4c-7 0-31.5 1.4-50.8 19.2a53.3 53.3 0 00-17.7 47.3v37.8H81.7a5 5 0 00-5 5V160a5 5 0 005 5z"
       />
     </svg>
   );
