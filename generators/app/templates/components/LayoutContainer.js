e08243e5bb6451679b49e8a9654c1406e0a86afe
TxHawks <tofu.hawks@gmail.com>
2020-07-14 14:09:25 +0300

    feat(app): use "grid" in Layout cotainer

--- generators/app/templates/components/LayoutContainer.js
+++ generators/app/templates/components/LayoutContainer.js
@@ -2,19 +2,41 @@ import React from 'react';
 import { useFela, } from 'react-fela';
 import { parseComponentProp, } from '@haaretz/htz-css-tools';
 
+
 export const styles = ({ bgc, namedBgc, theme, }) => ({
-  marginRight: 'auto',
-  marginLeft: 'auto',
-  width: '100%',
-  padding: '0 2rem',
+  paddingLeft: '2rem',
+  paddingRight: '2rem',
+
   extend: [
+    theme.mq({ from: 's'}, {
+      display: 'grid',
+      gridTemplateColumns: `2rem 1fr 2rem`,
+      paddingRight: '0',
+      paddingLeft: '0',
+
+      // By default, place each descendant element into
+      // the main column.
+      '& > *': {
+        gridColumn: '2',
+      },
+      '& > .full': {
+        gridColumn: '1 / -1',
+      },
+      '& > .pull-start': {
+        gridColumn: '1 / 2',
+      },
+      '& > .pull-end': {
+        gridColumn: '2 / 3',
+      },
+    }),
+
     parseComponentProp(
-      'maxWidth',
+      'gridTemplateColumns',
       [
-        { from: 's', until: 'm', value: 100, },
-        { from: 'm', until: 'l', value: 768 / 6, },
-        { from: 'l', until: 'xl', value: 1024 / 6, },
-        { from: 'xl', value: (1280 - 17) / 7, },
+        { from: 's', until: 'm', value: getColTemplate(100), },
+        { from: 'm', until: 'l', value: getColTemplate(768 / 6), },
+        { from: 'l', until: 'xl', value: getColTemplate(1024 / 6), },
+        { from: 'xl', value: getColTemplate((1280 - 17) / 7), },
       ],
       theme.mq
     ),
@@ -23,17 +45,27 @@ export const styles = ({ bgc, namedBgc, theme, }) => ({
 
 export default function LayoutContainer({
   attrs,
-  bgc,
-  namedBgc,
   children,
+  extraClasses,
   extraStyles,
   tagName = 'section',
 }) {
-  const className = useFela({ bgc, namedBgc, }).css(styles, extraStyles);
+  const className = useFela().css(styles, extraStyles);
   const Element = tagName;
+  const className = `${extraClasses ? extraClasses +  ' ' : ''}${className}`;
+
   return (
     <Element className={className} {...attrs}>
       {children}
     </Element>
   );
+}
+
+////////////////////////////////////////////////////////////////////////
+//                               UTILS                                //
+////////////////////////////////////////////////////////////////////////
+
+function getColTemplate(mainColWidth) {
+  const spacerCol = 'minmax(2rem, 1fr)';
+  return `${spacerCol} minmax(auto, ${mainColWidth}rem) ${spacerCol}`
 }
